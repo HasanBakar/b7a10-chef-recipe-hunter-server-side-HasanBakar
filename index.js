8bd6fa4fc005bdf180ddf9d3fddeabc5de72b55e
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-05-01 23:30:52 +0600

    initilly project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
+
+
+app.get('/', (req, res) =>{
+    res.send('Food Recipe is the identity of the chef.')
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
