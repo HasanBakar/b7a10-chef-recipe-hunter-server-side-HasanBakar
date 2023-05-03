b52836940a85ad7e4df33d8f5880a4fab0da8a9d
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-05-03 19:45:21 +0600

    created vercel.json for configaration

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
+
+const chefs = require('./data/top_chefs.json')
 
 
 app.get('/', (req, res) =>{
-    res.send('Food Recipe is the identity of the chef.')
+    res.send(chefs)
 });
 
 
